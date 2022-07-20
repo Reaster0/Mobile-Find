f86345cb0a792700c7891768636ea9d23491c9f5
jim <james.hawley@protonmail.com>
2022-07-20 16:03:32 +0200

    dark theme + repair rotation

--- app/src/main/java/com/free/ra_project/Utils.kt
+++ app/src/main/java/com/free/ra_project/Utils.kt
@@ -8,9 +8,7 @@ open class DirectionObjects(_image: ImageView) {
     var image: ImageView = _image
 
     fun rotate(_degree: Float) {
-        if (image.rotation + _degree >= 360)
-            image.rotation = (image.rotation + _degree) - 360
-        image.apply { rotation += _degree }
+        image.apply { rotation = _degree }
     }
 }
 
@@ -24,7 +22,7 @@ class Arrow(_image: ImageView) : DirectionObjects(_image) {
             1 / (maxRedDistance / _distance)
         image.setColorFilter(
             ColorUtils.blendARGB(
-                Color.parseColor("#2C2C2C"),
+                Color.parseColor("#9C9C9C"),
                 Color.parseColor("#2196F3"),
                 1 - ratio)) //between 0 and 1, 0 is 100% red, 1 is 100% blue
     }

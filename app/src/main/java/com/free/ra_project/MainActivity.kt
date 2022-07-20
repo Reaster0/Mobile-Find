57f815603e13dfa7bb4c04a3fdc841c88d062dee
earnaud <darcrai11@gmail.com>
2022-07-20 17:31:42 +0200

    small modification for the merge of the locationSensor and interface

--- app/src/main/java/com/free/ra_project/MainActivity.kt
+++ app/src/main/java/com/free/ra_project/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.free.ra_project
 
 import android.content.Context
-import android.hardware.SensorEvent
 import android.hardware.SensorManager
-import android.media.MicrophoneInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.free.ra_project.databinding.ActivityMainBinding
-import com.google.android.gms.location.*
 
 class MainActivity : AppCompatActivity(), GyroInterface, CompassInterface, LocationInterface {
 
@@ -18,16 +15,20 @@ class MainActivity : AppCompatActivity(), GyroInterface, CompassInterface, Locat
     var z = 0.0f
     lateinit var arrow : Arrow
     lateinit var compass : Compass
+    lateinit var sensorManager : SensorManager
 
     private lateinit var location : LocationSensor
+    private lateinit var gyroSensor : GyroSensor
+    private lateinit var compassSensor : CompassSensor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainScreenBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainScreenBinding.root)
-        var sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        var gyroSensor = GyroSensor(this, sensorManager)
-        var compassSensor = CompassSensor(this, sensorManager)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        gyroSensor = GyroSensor(this, sensorManager)
+        compassSensor = CompassSensor(this, sensorManager)
+
         compassSensor.startListen()
         gyroSensor.startListen()
         arrow = Arrow(mainScreenBinding.ivDirectionArrow)
@@ -36,8 +37,8 @@ class MainActivity : AppCompatActivity(), GyroInterface, CompassInterface, Locat
         mainScreenBinding.tvSavedCoordinates.text = getString(R.string.savedLocation, "", "")
 
         location = LocationSensor(this, this)
-        Log.d("testLog", "OnCreate done")
     }
+
 
     override fun locationValueUpdate(_latitude: Double, _longitude: Double, _altitude: Double) {
         mainScreenBinding.tvCurrentCoordinates.text = getString(R.string.currentLocation, _latitude.toString(), _longitude.toString())
@@ -48,13 +49,13 @@ class MainActivity : AppCompatActivity(), GyroInterface, CompassInterface, Locat
     }
     override fun gyroValueUpdate(_degree : Float) {
         x = _degree
-        mainScreenBinding.tvCurrentCoordinates.text = getString(R.string.angleDebug, x.toString())
+        //mainScreenBinding.tvCurrentCoordinates.text = getString(R.string.angleDebug, x.toString())
         arrow.rotate(_degree)
     }
 
     override fun compassValueUpdate(_degree : Float) {
         y = _degree
-        mainScreenBinding.tvSavedCoordinates.text = getString(R.string.angleDebug, _degree.toString())
+        //mainScreenBinding.tvSavedCoordinates.text = getString(R.string.angleDebug, _degree.toString())
         compass.rotate(_degree + 180 - x)
     }
 
@@ -62,16 +63,19 @@ class MainActivity : AppCompatActivity(), GyroInterface, CompassInterface, Locat
         super.onResume()
         mainScreenBinding.btnRegisterLocation.setOnClickListener {
             location.getLocation()
-            //getLocation()
             Log.d("testLog", "Button clicked")
         }
         location.startLocationUpdates()
+        gyroSensor.startListen()
+        compassSensor.startListen()
         Log.d("testLog", "OnResume done")
     }
 
     override fun onPause() {
         super.onPause()
         location.stopLocationUpdates()
+        gyroSensor.stopListen()
+        compassSensor.stopListen()
         Log.d("testLog", "onPause done")
     }
 
